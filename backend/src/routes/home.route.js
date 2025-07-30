499c8340de8e723a07d876d478091449199521b0
Sunny <spanyfox29@gmail.com>
2025-07-30 11:10:29 +0530

    add page

--- backend/src/routes/home.route.js
+++ backend/src/routes/home.route.js
@@ -3,7 +3,7 @@ import { add, edit } from "../controllers/home.controller.js"
 
 const router = express.Router()
 
-router.get("/add", add)
+router.get("/add page", add)
 router.get("/edit", edit)
 
 export default router
